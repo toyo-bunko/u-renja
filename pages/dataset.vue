69aecc1bdbd3a7920875c54bbfa61d9ae476db7c
nakamura196 <na.kamura.1263@gmail.com>
2021-07-05 14:36:10 +0900

    fix: bug

--- pages/dataset.vue
+++ pages/dataset.vue
@@ -68,18 +68,18 @@ export default class Page extends Vue {
     {
       label: '酉蓮社本（IIIFコレクション）',
       value:
-        'https://www.kanzaki.com/works/2016/pub/image-annotator?u=https://taishozo.github.io/u-renja/iiif2/collection/top.json',
+        'https://www.kanzaki.com/works/2016/pub/image-annotator?u=https://static.toyobunko-lab.jp/u-renja/iiif2/collection/top.json',
     },
     {
       label: '『大正新脩大蔵経勘同目録』（IIIFマニフェスト）',
       value:
-        'https://www.kanzaki.com/works/2016/pub/image-annotator?u=https://taishozo.github.io/db/iiif/kandomokuroku/manifest.json',
+        'https://www.kanzaki.com/works/2016/pub/image-annotator?u=https://static.toyobunko-lab.jp/taishozo/iiif/kandomokuroku/manifest.json',
     },
     /*
     {
       label: 'メタデータ一覧（Excel）',
       value:
-        'https://view.officeapps.live.com/op/view.aspx?src=https://taishozo.github.io/u-renja/metadata/data.xlsx',
+        'https://view.officeapps.live.com/op/view.aspx?src=https://static.toyobunko-lab.jp/u-renja/metadata/data.xlsx',
     },
     */
   ]
